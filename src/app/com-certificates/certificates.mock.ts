13219607393a04a71110d31ce491b5599e2233ae
Daniel Enriquez <rdanienriquez01@gmail.com>
2026-01-10 13:12:43 -0500

    se actualiza la hoja de vida, los botones de los certificados yel botón de ig

--- src/app/com-certificates/certificates.mock.ts
+++ src/app/com-certificates/certificates.mock.ts
@@ -1,33 +1,39 @@
 export const certificatesList: Certificate[] = [
     {
-        img: '../../assets/img/certificado1.jpg',
+        img: '../../assets/img/certificado-php.jpg',
         alt: 'Certificado Udemy',
-        title: 'Backend'
+        title: 'Backend',
+        url: 'https://www.udemy.com/certificate/UC-d613dd77-6a71-40f1-817b-db563af812c0/'
     },
     {
-        img: '../../assets/img/certificado8.jpg',
+        img: '../../assets/img/certificado-java.jpg',
         alt: 'Certificado Udemy',
-        title: 'Backend'
+        title: 'Backend',
+        url: 'https://www.udemy.com/certificate/UC-bec812b1-40c3-4c0b-9317-e7f07f9bb4dd/'
     },
     {
-        img: '../../assets/img/certificado4.jpg',
+        img: '../../assets/img/certificado-mean.jpg',
         alt: 'Certificado Udemy',
-        title: 'Backend'
+        title: 'Backend',
+        url: 'https://www.udemy.com/certificate/UC-a6a2f0aa-6031-487f-ad65-9dae7bd70b45/'
     },
     {
-        img: '../../assets/img/certificado7.jpg',
+        img: '../../assets/img/certificado-angular.jpg',
         alt: 'Certificado Udemy',
-        title: 'Fronted'
+        title: 'Fronted',
+        url: 'https://www.udemy.com/certificate/UC-cfb3f0d6-2877-43b2-858d-71b6e45751e4/'
     },
     {
-        img: '../../assets/img/certificado12.jpg',
+        img: '../../assets/img/certificado-flutter.jpg',
         alt: 'Certificado Udemy',
-        title: 'Móvil'
+        title: 'Móvil',
+        url: 'https://www.udemy.com/certificate/UC-3c4382ac-2491-4f2b-9415-d331c360d7f4/'
     },
     {
-        img: '../../assets/img/certificado10.jpg',
+        img: '../../assets/img/certificado-hacking.jpg',
         alt: 'Certificado Udemy',
-        title: 'Ciberseguridad'
+        title: 'Ciberseguridad',
+        url: 'https://www.udemy.com/certificate/UC-7714a0ae-0284-4264-906c-e929d5977904/'
     }
 ]
 
@@ -35,4 +41,5 @@ export interface Certificate {
   img: String;
   alt: String;
   title: String;
+  url: String;
 }
--- src/app/com-certificates/com-certificates.component.ts
+++ src/app/com-certificates/com-certificates.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewEncapsulation, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
-import { certificatesList } from './certificates.mock';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { certificatesList, Certificate } from './certificates.mock';
 
 @Component({
   selector: 'app-com-certificates',
@@ -10,33 +10,11 @@ import { certificatesList } from './certificates.mock';
   encapsulation: ViewEncapsulation.None
 })
 
-export class ComCertificatesComponent implements AfterViewInit {
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit() {
-    const modal = this.el.nativeElement.querySelector('#myModal');
-    const modalImg = this.el.nativeElement.querySelector('#modalImg');
-    const projectsCards = this.el.nativeElement.querySelectorAll('.img-certificates');
-    const closeModal = this.el.nativeElement.querySelector('.close');
-
-    projectsCards.forEach((card: HTMLElement) => {
-      this.renderer.listen(card, 'click', () => {
-        this.renderer.setStyle(modal, 'display', 'block');
-        modalImg.src = card.querySelector('img')!.src;
-      });
-    });
-
-    this.renderer.listen(closeModal, 'click', () => {
-      this.renderer.setStyle(modal, 'display', 'none');
-    });
-
-    this.renderer.listen('window', 'click', (event) => {
-      if (event.target === modal) {
-        this.renderer.setStyle(modal, 'display', 'none');
-      }
-    });
-  }
+export class ComCertificatesComponent {
 
   certificateList = certificatesList;
+
+  openCertificate(certificate: Certificate) {
+    window.open(certificate.url as string, '_blank');
+  }
 }
